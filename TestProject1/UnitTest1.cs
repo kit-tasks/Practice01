96a53e2630b59ca7c0b563436354f498c3045be4
maxekb <maxekb@live.ru>
2024-03-13 08:12:02 +0300

    start unit test

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using OnlineShop;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var item = new Class1();
+            Assert.Pass();
+        }
+    }
+}
